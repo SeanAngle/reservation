8ecd642b0bcb494c3a7a131b0af4e5a910334a04
WhoisJohnKim <johnkim9417@gmail.com>
2018-08-28 13:20:22 -0500

    Setting up express server

---
+++ server.js
@@ -0,0 +1,53 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var tables = [];
+var waitlist = [];
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/view", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/make", function(req, res) {
+    res.sendFile(path.join(__dirname, "make.html"));
+});
+
+app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+});
+
+app.post("/api/tables", function(req, res){
+    var newTable = req.body;
+    newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newTable);
+    tables.push(newTable);
+    res.json(newTable);
+})
+
+app.post("/api/waitlist", function(req, res){
+    var newWaitlist = req.body;
+    newWaitlist.routeName = newWaitlist.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newWaitlist);
+    waitlist.push(newWaitlist);
+    res.json(newWaitlist);
+})
